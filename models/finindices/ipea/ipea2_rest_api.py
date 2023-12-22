1887faada184f7a5a5fbde130d0dba78944ca729
Luiz Lewis <livrosetc@yahoo.com.br>
2023-12-22 20:07:43 -0300

    Minor changes for pulling remote repo and continuing;

---
+++ models/finindices/ipea/ipea2_rest_api.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+models/finindices/ipea/ipea2_rest_api.py
+
+  Metadados('{SERCODIGO}')/Valores
+    ou
+  ValoresSerie(SERCODIGO='{SERCODIGO}')
+
+
+Descrição da REST API disponível:
+  http://www.ipeadata.gov.br/api
+
+http://www.ipeadata.gov.br/Default.aspx
+https://pypi.org/project/ipeadatapy/
+"""
+
+
+
+
+
+def adhoctest():
+  pass
+
+
+def process():
+  pass
+
+
+if __name__ == '__main__':
+  """
+  adhoctest()
+  """
+  process()
